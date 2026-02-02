39b49ce4f6ae88f54b1ce9e7e4f1e3a6726887ac
Jp12game <jos.pol.h@gmail.com>
2026-02-01 21:58:32 -0600

    botones funcionales

--- Recursos practica 2/Recursos practica 2/Recursos practica 2/carrito-compras/src/App.jsx
+++ Recursos practica 2/Recursos practica 2/Recursos practica 2/carrito-compras/src/App.jsx
@@ -7,43 +7,66 @@ import { db } from "./data/db"
 
 export const App = () => {
 
-    function initialCart(){
-        const localStorageCart=localStorage.getItem('cart')
-        return localStorageCart ?   JSON.parse(localStorageCart) : []
+    function initialCart() {
+        const localStorageCart = localStorage.getItem('cart')
+        return localStorageCart ? JSON.parse(localStorageCart) : []
     }
 
     const [data, setData] = useState(db);
     const [cart, setCart] = useState(initialCart);
 
-    useEffect(() =>{
+    useEffect(() => {
         localStorage.setItem('cart', JSON.stringify(cart))
     }, [cart])
 
     function addToCart(guitar) {
-        const itemIndex = cart.findIndex((item) =>guitar.id === item.id)
+        const itemIndex = cart.findIndex((item) => guitar.id === item.id)
         console.log(itemIndex);
-        if(itemIndex===-1){
-            guitar.quantity=1;
+        if (itemIndex === -1) {
+            guitar.quantity = 1;
             setCart([...cart, guitar])
-        }else{
+        } else {
             const updatedCart = [...cart]
             updatedCart[itemIndex].quantity++;
             setCart(updatedCart);
         }
     }
 
-    function calculateTotal(){
+    function removeFromCart(guitar) {
+        const itemIndex = cart.findIndex((item) => guitar.id === item.id)
+        console.log(itemIndex);
+        const updatedCart = [...cart]
+        updatedCart[itemIndex].quantity--;
+        if (updatedCart[itemIndex].quantity === 0) {
+            updatedCart.splice(itemIndex, 1);
+        }
+        setCart(updatedCart);
+    }
+
+    function deleteFromCart(guitar) {
+        const itemIndex = cart.findIndex((item) => guitar.id === item.id)
+        console.log(itemIndex);
+        const updatedCart = [...cart]
+        updatedCart.splice(itemIndex, 1);
+        setCart(updatedCart);
+    }
+
+    function clearCart() {
+        setCart([]);
+    }
+
+    function calculateTotal() {
         /*let total = 0;
         for (const guiatr of cart) {
             total+=guiatr.price * guiatr.quantity;
         }*/
-        let total = cart.reduce((total, item)=>total+=item.price*item.quantity, 0)
+        let total = cart.reduce((total, item) => total += item.price * item.quantity, 0)
         return total;
     }
 
     return (
         <>
-            <Header cart={cart} total = {calculateTotal()}/>
+            <Header cart={cart} total={calculateTotal()} addToCart = {addToCart} removeFromCart = {removeFromCart} deleteFromCart = {deleteFromCart} clearCart={clearCart} />
             <main className="container-xl mt-5">
                 <h2 className="text-center">Nuestra Colección</h2>
 
--- Recursos practica 2/Recursos practica 2/Recursos practica 2/carrito-compras/src/componentes/Header.jsx
+++ Recursos practica 2/Recursos practica 2/Recursos practica 2/carrito-compras/src/componentes/Header.jsx
@@ -1,5 +1,5 @@
 
-export const Header = ({ cart, total }) => {
+export const Header = ({ cart, total, addToCart, removeFromCart, deleteFromCart, clearCart }) => {
     return (
         <header className="py-5 header">
             <div className="container-xl">
@@ -41,6 +41,7 @@ export const Header = ({ cart, total }) => {
                                                     <button
                                                         type="button"
                                                         className="btn btn-dark"
+                                                        onClick={()=>removeFromCart(guitar)}
                                                     >
                                                         -
                                                     </button>
@@ -48,6 +49,7 @@ export const Header = ({ cart, total }) => {
                                                     <button
                                                         type="button"
                                                         className="btn btn-dark"
+                                                        onClick={()=>addToCart(guitar)}
                                                     >
                                                         +
                                                     </button>
@@ -56,6 +58,7 @@ export const Header = ({ cart, total }) => {
                                                     <button
                                                         className="btn btn-danger"
                                                         type="button"
+                                                        onClick={()=>deleteFromCart(guitar)}
                                                     >
                                                         X
                                                     </button>
@@ -66,7 +69,7 @@ export const Header = ({ cart, total }) => {
                                 </table>
                                 }
                                 <p className="text-end">Total pagar: <span className="fw-bold">${total}</span></p>
-                                <button className="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
+                                <button className="btn btn-dark w-100 mt-3 p-2" onClick={()=>clearCart()}>Vaciar Carrito</button>
                             </div>
                         </div>
                     </nav>
